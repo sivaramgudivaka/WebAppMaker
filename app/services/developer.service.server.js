eb2ee0c04a348fae354bd63e42e00f9ddef24ced
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-28 20:13:32 -0400

    Fixed - renamed findUserByCredentials with findDeveloperByCredentials

--- app/services/developer.service.server.js
+++ app/services/developer.service.server.js
@@ -15,7 +15,7 @@ module.exports = function (app, developerModel) {
 
     function localStrategy(username, password, done) {
         developerModel
-            .findUserByCredentials({username: username, password: password})
+            .findDeveloperByCredentials({username: username, password: password})
             .then(
                 function(user) {
                     if (!user) { return done(null, false); }
