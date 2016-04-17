628ac10146a38fa79752a77d52f2721d4bf2d021
Jose Annunziato <jannunzi@gmail.com>
2016-04-17 07:30:01 -0400

    SECURITY - moved developer views into "developers" folder in preparation of creating a separate angular app for "users"

--- public/config.js
+++ public/config.js
@@ -7,36 +7,36 @@
         $routeProvider
             // authentication routes
             .when ("/", {
-                templateUrl: "views/security/login.view.html",
+                templateUrl: "views/developers/security/login.view.html",
                 controller: "LoginController",
                 controllerAs: "model"
             })
             .when ("/register", {
-                templateUrl: "views/security/register.view.html",
+                templateUrl: "views/developers/security/register.view.html",
                 controller: "RegisterController",
                 controllerAs: "model"
             })
             // developer routes
             .when ("/developer", {
-                templateUrl: "views/developer/developer-list.view.html",
+                templateUrl: "views/developers/developer/developer-list.view.html",
                 controller: "DeveloperListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/new", {
-                templateUrl: "views/developer/developer-new.view.html",
+                templateUrl: "views/developers/developer/developer-new.view.html",
                 controller: "NewDeveloperController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/edit", {
-                templateUrl: "views/developer/developer-edit.view.html",
+                templateUrl: "views/developers/developer/developer-edit.view.html",
                 controller: "EditDeveloperController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/profile", {
-                templateUrl: "views/developer/developer-edit.view.html",
+                templateUrl: "views/developers/developer/developer-edit.view.html",
                 controller: "EditDeveloperController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
@@ -44,50 +44,50 @@
 
             // application routes
             .when ("/developer/:username/application", {
-                templateUrl: "views/application/application-list.view.html",
+                templateUrl: "views/developers/application/application-list.view.html",
                 controller: "ApplicationListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/new", {
-                templateUrl: "views/application/application-new.view.html",
+                templateUrl: "views/developers/application/application-new.view.html",
                 controller: "NewApplicationController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/edit/:applicationId", {
-                templateUrl: "views/application/application-edit.view.html",
+                templateUrl: "views/developers/application/application-edit.view.html",
                 controller: "EditApplicationController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/edit", {
-                templateUrl: "views/application/application-edit.view.html",
+                templateUrl: "views/developers/application/application-edit.view.html",
                 controller: "EditApplicationController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/share", {
-                templateUrl: "views/application/application-share.view.html",
+                templateUrl: "views/developers/application/application-share.view.html",
                 controller: "ShareApplicationController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             // page routes
             .when ("/developer/:username/application/:applicationId/page", {
-                templateUrl: "views/page/page-list.view.html",
+                templateUrl: "views/developers/page/page-list.view.html",
                 controller: "PageListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/new", {
-                templateUrl: "views/page/page-new.view.html",
+                templateUrl: "views/developers/page/page-new.view.html",
                 controller: "NewPageController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/edit", {
-                templateUrl: "views/page/page-edit.view.html",
+                templateUrl: "views/developers/page/page-edit.view.html",
                 controller: "EditPageController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
@@ -95,63 +95,63 @@
 
             // widget routes
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget", {
-                templateUrl: "views/widget/widget-list.view.html",
+                templateUrl: "views/developers/widget/widget-list.view.html",
                 controller: "WidgetListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/run", {
-                templateUrl: "views/page/page-run.view.html",
+                templateUrl: "views/developers/page/page-run.view.html",
                 controller: "PageRunController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/preview", {
-                templateUrl: "views/widget/page-preview.view.html",
+                templateUrl: "views/developers/widget/page-preview.view.html",
                 controller: "WidgetListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/choose-widget", {
-                templateUrl: "views/widget/widget-choose.view.html",
+                templateUrl: "views/developers/widget/widget-choose.view.html",
                 controller: "ChooseWidgetController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/edit", {
-                templateUrl: "views/widget/widget-edit.view.html",
+                templateUrl: "views/developers/widget/widget-edit.view.html",
                 controller: "WidgetEditController",
                 controllerAs: "model"
             })
 
             // script routes
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/script", {
-                templateUrl: "views/script/script-list.view.html",
+                templateUrl: "views/developers/script/script-list.view.html",
                 controller: "ScriptListController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/script/new", {
-                templateUrl: "views/script/script-new.view.html",
+                templateUrl: "views/developers/script/script-new.view.html",
                 controller: "NewScriptController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/script/edit", {
-                templateUrl: "views/script/script-edit.view.html",
+                templateUrl: "views/developers/script/script-edit.view.html",
                 controller: "EditScriptController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/script/choose", {
-                templateUrl: "views/script/statement-choose.view.html",
+                templateUrl: "views/developers/script/statement-choose.view.html",
                 controller: "ChooseStatementController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
             })
             // navigate to statement editor when you click on th cog
             .when ("/developer/:username/application/:applicationId/page/:pageId/widget/:widgetId/script/statement/:statementId", {
-                templateUrl: "views/script/statement-edit.view.html",
+                templateUrl: "views/developers/script/statement-edit.view.html",
                 controller: "EditStatementController",
                 controllerAs: "model",
                 resolve: { loggedin: checkLoggedin }
---
+++ public/views/developers/security/security.controller.js
@@ -0,0 +1,47 @@
+(function(){
+    angular
+        .module("WebAppMakerApp")
+        .controller("RegisterController", registerController)
+        .controller("LoginController", loginController);
+
+    function registerController(SecurityService, $rootScope, $location) {
+        var vm = this;
+        vm.register = register;
+
+        function register(developer) {
+            delete developer.password2;
+            SecurityService
+                .register(developer)
+                .then(
+                    function(response) {
+                        if(response.data) {
+                            $rootScope.currentUser = response.data;
+                            $location.url("/developer/"+developer.username+"/edit");
+                        }
+                    },
+                    function(err) {
+                        vm.error = err;
+                    }
+                );
+        }
+    }
+
+    function loginController(SecurityService, $rootScope, $location) {
+        var vm = this;
+        vm.login = login;
+
+        function login(developer) {
+            SecurityService
+                .login(developer)
+                .then(
+                    function(response) {
+                        $rootScope.currentUser = response.data;
+                        $location.url("/developer/"+developer.username+"/edit");
+                    },
+                    function(err) {
+                        vm.error = err;
+                    }
+                );
+        }
+    }
+})();
