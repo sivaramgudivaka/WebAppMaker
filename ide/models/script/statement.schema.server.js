5ac0dead66c480ac94624b8cfbfdc46021526237
Jose Annunziato <jannunzi@gmail.com>
2016-05-23 23:47:54 -0400

    added new types of script statements: DATE, BOOLEAN, ARRAY, OBJECT

--- ide/models/script/statement.schema.server.js
+++ ide/models/script/statement.schema.server.js
@@ -6,7 +6,7 @@ module.exports = function() {
     var NumberStatementSchema = require("./number-statement.schema.server")();
 
     var StatementSchema = mongoose.Schema({
-        statementType   : {type : String, enum : ["NUMBER", "STRING", "DATE", "OBJECT", "DATABASE", "LOGIC", "DECISION"]},
+        statementType   : {type : String, enum : ["NUMBER", "STRING", "DATE", "OBJECT", "ARRAY", "DATABASE", "BOOLEAN", "DECISION"]},
         variables: [String],
         title: String,
         numberStatement : NumberStatementSchema
