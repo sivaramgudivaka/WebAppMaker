49ba545aff6f446c3911a7c8696a040d82eb3128
Aditya Wate <aditya.wate@hotmail.com>
2016-06-18 22:11:12 -0400

    string statement: first draft

--- public/ide/views/statement/statement.controller.js
+++ public/ide/views/statement/statement.controller.js
@@ -78,6 +78,13 @@
             {label: 'Navigate to page'}
         ];
 
+        vm.stringOperations = [
+            {label: 'Substring'},
+            {label: 'Concatenate'},
+            {label: 'Length'}
+        ];
+        vm.stringOperation = vm.stringOperations[0];
+
         // route params
         vm.username    = $routeParams.username;
         vm.developerId = $routeParams.developerId;
