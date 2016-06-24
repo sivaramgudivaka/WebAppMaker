6c50dfeab25def5cb4fbe16996da63afae14c4f6
Aditya Wate <aditya.wate@hotmail.com>
2016-06-24 08:07:22 -0400

    String schema

--- ide/models/statement/string-statement.schema.server.js
+++ ide/models/statement/string-statement.schema.server.js
@@ -3,12 +3,18 @@ var mongoose = require("mongoose");
 module.exports = function() {
 
     var StringStatementSchema = mongoose.Schema({
-        operationType : {type : String, enum : ["SUBSTRING", "CONCATENATE", "LENGTH"]},
+        operationType : {type : String, enum : ["SUBSTRING", "CONCATENATE", "LENGTH","CHARAT","INDEXOF","LASTINDEXOF",
+            "SEARCH","REPEAT","REPLACE","SLICE","LOWERCASE","UPPERCASE","TRIM",
+            "STARTSWITH", "ENDSWITH"]},
         input1        : String,
         input2        : String,
         output        : String,
         length        : String,
-        start         : String
+        start         : String,
+        index         : String,
+        searchvalue   : String,
+        newvalue      : String,
+        count         : String
     });
 
     return StringStatementSchema;
