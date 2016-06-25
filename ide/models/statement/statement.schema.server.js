8b08425d86937fd86320c199ae4b49a6138e7a04
swapnilkmahajan <mahajan.sw@husky.neu.edu>
2016-06-25 15:48:17 -0400

    IfStatement schema added to Parent Statement schema

--- ide/models/statement/statement.schema.server.js
+++ ide/models/statement/statement.schema.server.js
@@ -4,14 +4,17 @@ var mongoose = require("mongoose");
 module.exports = function() {
 
     var StatementSchema = mongoose.Schema({
-        _script : {type: mongoose.Schema.ObjectId, ref: 'Script'},
+        _script : {type: mongoose.Schema.ObjectId, ref: 'ScriptModel'}, //AW: Changed Script to ScriptModel
+        name: String,
         statementType   : {type : String, enum : ["NUMBER", "STRING", "DATE", "OBJECT", "ARRAY", "DATABASE", "BOOLEAN", "DECISION"]},
         variables: [String],
         title: String,
         databaseStatement : require("./database-statement.schema.server")(),
         numberStatement : require("./number-statement.schema.server")(),
         dateStatement   : require("./date-statement.schema.server")(),
-        stringStatement   : require("./string-statement.schema.server")()
+        stringStatement   : require("./string-statement.schema.server")(),
+        ifStatement: require("./if-statement.schema.server")(),
+        booleanStatement: require("./boolean-statement.schema.server")()
     }, {collection: 'statement'});
 
     return StatementSchema;
