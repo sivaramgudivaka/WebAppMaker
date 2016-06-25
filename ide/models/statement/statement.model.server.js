39298dc2751c66f14c89dcd4adab7165e10638c0
swapnilkmahajan <mahajan.sw@husky.neu.edu>
2016-06-25 15:51:01 -0400

    added findAllStatements functionality for the script in the scope.  Modified Controller, client service, server service and statement model

--- ide/models/statement/statement.model.server.js
+++ ide/models/statement/statement.model.server.js
@@ -10,44 +10,6 @@ module.exports = function(models) {
         deleteStatement: deleteStatement
     };
     return api;
-    
-    function findAllStatements(scope){
-        var scriptId = scope.scriptId;
-        return Statement
-            .find({_script:scriptId})
-            .then(
-                function(statements){
-                    return statements;
-                },
-                function(err){
-                    console.log(err);
-                }
-            );
-    }
-<<<<<<< ae2d46846ca1e1ef88fc86a4d01adb320cbc647f
-
-    function deleteStatement(scope) {
-        return Statement
-            .remove(
-                {
-                    _id: scope.statementId
-                }
-            );
-    }
-
-    function findAllStatements(scope){
-        var scriptId = scope.scriptId;
-        return Statement
-            .find({_script:scriptId})
-            .then(
-                function(statements){
-                    return statements;
-                },
-                function(err){
-                    console.log(err);
-                }
-            );
-    }
 
     function findAllStatements(scope){
         var scriptId = scope.scriptId;
@@ -71,8 +33,43 @@ module.exports = function(models) {
                 }
             );
     }
-=======
->>>>>>> added findAllStatements functionality for the script in the scope.
+
+    function findAllStatements(scope){
+        var scriptId = scope.scriptId;
+        return Statement
+            .find({_script:scriptId})
+            .then(
+                function(statements){
+                    return statements;
+                },
+                function(err){
+                    console.log(err);
+                }
+            );
+    }
+
+    function findAllStatements(scope){
+        var scriptId = scope.scriptId;
+        return Statement
+            .find({_script:scriptId})
+            .then(
+                function(statements){
+                    return statements;
+                },
+                function(err){
+                    console.log(err);
+                }
+            );
+    }
+
+    function deleteStatement(scope) {
+        return Statement
+            .remove(
+                {
+                    _id: scope.statementId
+                }
+            );
+    }
 
     function findAllStatements(scope){
         var scriptId = scope.scriptId;
