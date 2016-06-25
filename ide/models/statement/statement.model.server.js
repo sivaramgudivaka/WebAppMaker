93737a4b7ab45a127e18609abc4ebe7c22225185
swapnilkmahajan <mahajan.sw@husky.neu.edu>
2016-06-25 15:51:01 -0400

    added findAllStatements functionality for the script in the scope.  Modified Controller, client service, server service and statement model

--- ide/models/statement/statement.model.server.js
+++ ide/models/statement/statement.model.server.js
@@ -10,6 +10,20 @@ module.exports = function(models) {
         deleteStatement: deleteStatement
     };
     return api;
+    
+    function findAllStatements(scope){
+        var scriptId = scope.scriptId;
+        return Statement
+            .find({_script:scriptId})
+            .then(
+                function(statements){
+                    return statements;
+                },
+                function(err){
+                    console.log(err);
+                }
+            );
+    }
 
     function findAllStatements(scope){
         var scriptId = scope.scriptId;
