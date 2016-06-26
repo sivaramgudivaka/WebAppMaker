34874b70e23e57dda85f3a4f25538e9a748605d1
rushibadami1992 <rushibadami1992@gmail.com>
2016-06-25 23:23:11 -0400

    Done with schema change ,Added Gallery Views,Left Google Api linking,Client Controller,Bussiness logic

--- ide/models/widget/image.schema.server.js
+++ ide/models/widget/image.schema.server.js
@@ -3,10 +3,11 @@ var mongoose = require("mongoose");
 module.exports = function () {
 
     var ImageSchema = mongoose.Schema({
+        image_Id: { type: mongoose.Schema.Types.ObjectId, ref: 'ImageGallery',default: null},
         url   : String,
         width : String,
         height: String
-        
+
     });
 
     return ImageSchema;
