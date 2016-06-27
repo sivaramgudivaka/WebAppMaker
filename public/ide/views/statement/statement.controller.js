f901dba81ce6a99fe1cd3a58de5703566ee9fe90
Aditya Wate <aditya.wate@hotmail.com>
2016-06-27 12:36:45 -0400

    statement controller master merge

--- public/ide/views/statement/statement.controller.js
+++ public/ide/views/statement/statement.controller.js
@@ -160,10 +160,9 @@
         }
 
         function saveStatement() {
+            console.log(vm.statement);
 
 
-
-            //AW: TODO: Remove this commenting
             //vm.statement.dateStatement.dateOperation = vm.statement.dateStatement.dateOperation.label;
             vm.statement.statementType = vm.statementType.label.toUpperCase();
             delete vm.statementType;
@@ -177,7 +176,7 @@
             //AW: Specific to String statements
             if(vm.statement.statementType === "STRING")
                 vm.statement.stringStatement.operationType = vm.stringOperation.label.toUpperCase();
-            
+
             StatementService
                 .saveStatement(vm, vm.statement)
                 .then(
