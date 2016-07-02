ae2d46846ca1e1ef88fc86a4d01adb320cbc647f
swapnilkmahajan <mahajan.sw@husky.neu.edu>
2016-07-02 15:15:42 -0400

    Boolean Schema added to statement schema

--- public/ide/views/statement/statement.controller.js
+++ public/ide/views/statement/statement.controller.js
@@ -220,6 +220,11 @@
 
             if (vm.statementType.label === "If")
                 vm.statement.ifStatement.comparator = vm.statement.ifStatement.comparator.label;
+            
+            /*if (vm.statementType.label === "Boolean"){
+                if(vm.statement.booleanStatement.input1 === 'NOT'){
+                }
+            }*/
 
 
             /*if (vm.statementType.label === "Boolean"){
