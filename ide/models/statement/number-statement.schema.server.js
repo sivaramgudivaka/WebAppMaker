d2a85dc71f104ca0890a65a81263716570ad77ad
isha <srivastava.i@husky.neu.edu>
2016-07-02 14:15:27 -0700

    modifying and added new operations

--- ide/models/statement/number-statement.schema.server.js
+++ ide/models/statement/number-statement.schema.server.js
@@ -3,7 +3,8 @@ var mongoose = require("mongoose");
 module.exports = function() {
 
     var NumberStatementSchema = mongoose.Schema({
-        operationType : {type : String, enum : ["+", "-", "x", "/"]},
+        operationType : {type : String, enum : ["+", "-", "x", "/","log","power","square root","cube root","%","sin","cos",
+        "tan","cot","cosec", "cosine","cube"]},
         input1        : String,
         input2        : String,
         output        : String
@@ -11,3 +12,4 @@ module.exports = function() {
 
     return NumberStatementSchema;
 };
+
